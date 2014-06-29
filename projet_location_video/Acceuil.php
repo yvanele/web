d51a027c770baafb00d7eb81f5f459084a9883bd
yvanele <yvanlloyd@hotmail.fr>
2014-06-29 17:09:31 +0200

    Create Acceuil.php
    Page d'acceuil du site de location de films

---
+++ projet_location_video/Acceuil.php
@@ -0,0 +1,17 @@
+<?php
+	include('Outils.inc');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN" 
+	"http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<title>VideoEXPRESS</title>
+		<link rel='stylesheet' media='screen' href='Deroule.css' />
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	</head>
+	<body>
+		<?php echo banniere('Accueil.php', 'Yvan'); ?>
+	</body>
+</html>
