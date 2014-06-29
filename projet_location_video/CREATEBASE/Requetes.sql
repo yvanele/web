f0a8cf4bd4373aa88ea33b6d4e89ab091e2bfa5c
yvanele <yvanlloyd@hotmail.fr>
2014-06-29 17:45:05 +0200

    Create Requetes.sql
    Fichier permettant d'initialiser les cassettes en les attribuant à un abonné (nécessaire pour la phase de tests) avec le statut "emprunté"

---
+++ projet_location_video/CREATEBASE/Requetes.sql
@@ -0,0 +1,87 @@
+UPDATE ABONNES SET NbCassettes = 9 WHERE Code = '1Xu123';
+
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 1 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 2 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 3 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 4 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 5 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 6 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 7 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 8 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 9 AND NoExemplaire = 1;
+
+INSERT INTO EMPRES VALUES(1, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(2, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(3, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(4, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(5, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(6, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(7, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(8, 1, '1Xu123', NOW());
+INSERT INTO EMPRES VALUES(9, 1, '1Xu123', NOW());
+
+UPDATE ABONNES SET NbCassettes = 9 WHERE Code = '25y13p';
+
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 1 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 2 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 3 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 4 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 5 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 6 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 7 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 8 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 9 AND NoExemplaire = 2;
+
+INSERT INTO EMPRES VALUES(1, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(2, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(3, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(4, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(5, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(6, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(7, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(8, 2, '25y13p', NOW());
+INSERT INTO EMPRES VALUES(9, 2, '25y13p', NOW());
+
+UPDATE ABONNES SET NbCassettes = 9 WHERE Code = '365AL8';
+
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 10 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 11 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 12 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 13 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 14 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 15 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 16 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 17 AND NoExemplaire = 1;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 18 AND NoExemplaire = 1;
+
+INSERT INTO EMPRES VALUES(10, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(11, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(12, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(13, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(14, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(15, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(16, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(17, 1, '365AL8', NOW());
+INSERT INTO EMPRES VALUES(18, 1, '365AL8', NOW());
+
+UPDATE ABONNES SET NbCassettes = 9 WHERE Code = '4367Xs';
+
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 10 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 11 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 12 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 13 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 14 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 15 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 16 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 17 AND NoExemplaire = 2;
+UPDATE CASSETTES SET Statut = 'empruntee' WHERE NoFilm = 18 AND NoExemplaire = 2;
+
+INSERT INTO EMPRES VALUES(10, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(11, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(12, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(13, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(14, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(15, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(16, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(17, 2, '4367Xs', NOW());
+INSERT INTO EMPRES VALUES(18, 2, '4367Xs', NOW());
