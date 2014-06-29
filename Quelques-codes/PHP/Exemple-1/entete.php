a6e2c00efb902ee7a9962343362a8d6064a742e6
yvanele <yvanlloyd@hotmail.fr>
2014-06-29 19:22:15 +0200

    Create entete.php

---
+++ Quelques-codes/PHP/Exemple-1/entete.php
@@ -0,0 +1,11 @@
+<?php
+define('DOCTYPE_XHTML_BASIC_11',
+	 ' <!DOCTYPE html PUBLIC 
+         "-//W3C//DTD XHTML BASIC 1.1//EN"
+         "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd"> ');
+
+define('HTML_FR', '<html lang="fr" xmlns="http://www.w3.org/1999/xhtml">');
+
+define('META_TYPE_TEXT_UTF8', 
+		'<meta http-equiv="content-type" content="text/html; charset=UTF-8">');
+?>
